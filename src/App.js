57610394a155ec2d4de727d2db451e5ac4e7c19f
Snehal Shah <snehal@yourwanda.com>
2017-04-18 10:59:42 -0700

    Initial working commit. Added router, home page
    1. Added router
    2. Added images
    3. Added link for contact me

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Slider} from './demo';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+       new Slider();
+    }
+
+    render() {
+        return (
+            <main>
+                <div className="page-view">
+                    <div className="project">
+                        <div className="text">
+                            <p>Highway</p>
+                        </div>
+                    </div>
+                    <div className="project">
+                        <div className="text">
+                            <p>Carmel Beach</p>
+                        </div>
+                    </div>
+                    <div className="project">
+                        <div className="text">
+                            <p>Half Moon Bay</p>
+                        </div>
+                    </div>
+                    <div className="project">
+                        <div className="text">
+                            <p>The Lone Tree</p>
+                        </div>
+                    </div>
+                    <nav className="arrows">
+                        <div className="arrow previous">
+                            <svg viewBox="208.3 352 4.2 6.4">
+                                <polygon className="st0"
+                                         points="212.1,357.3 211.5,358 208.7,355.1 211.5,352.3 212.1,353 209.9,355.1"/>
+                            </svg>
+                        </div>
+                        <div className="arrow next">
+                            <svg viewBox="208.3 352 4.2 6.4">
+                                <polygon className="st0"
+                                         points="212.1,357.3 211.5,358 208.7,355.1 211.5,352.3 212.1,353 209.9,355.1"/>
+                            </svg>
+                        </div>
+                    </nav>
+                </div>
+            </main>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Couple from './Couple';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+
+ReactDOM.render((
+        <Router>
+            <div>
+                <header className="codrops-header">
+                    <h1 className="codrops-header__title">
+                        <Link to="/">JS Fotografia</Link>
+                    </h1>
+                    <p className="codrops-header__demos">
+                        <Link to="/couple">Pre-Wedding</Link>
+                    </p>
+                    <p className="codrops-header__demos">
+                        <a href="mailto:jinen@jsfotografia.com">Contact Me</a>
+                    </p>
+                    <p className="message">You're seeing a fallback because your browser does not support CSS Masks.</p>
+                </header>
+                <hr/>
+
+                <Route exact path="/" component={App}/>
+                <Route path="/couple" component={Couple}/>
+            </div>
+        </Router>
+    ),
+    document.getElementById('root')
 );
